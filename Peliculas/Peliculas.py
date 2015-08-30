2cdc29ba6b60175e9dd76c8ed4720e76b1859fe8
Fabyfabs <bam_omega3@hotmail.com>
2015-08-30 00:41:55 -0300

    Controlador ,Modelo y Gui
    Falta cambiar el path de la base de datos

---
+++ Peliculas/Peliculas.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'Peliculas.ui'
+#
+# Created: Sat Aug 29 23:23:48 2015
+#      by: pyside-uic 0.2.15 running on PySide 1.2.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PySide import QtCore, QtGui
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(541, 444)
+        self.btn_eliminar = QtGui.QPushButton(Form)
+        self.btn_eliminar.setGeometry(QtCore.QRect(10, 20, 98, 27))
+        self.btn_eliminar.setObjectName("btn_eliminar")
+        self.btn_editar = QtGui.QPushButton(Form)
+        self.btn_editar.setGeometry(QtCore.QRect(250, 20, 98, 27))
+        self.btn_editar.setObjectName("btn_editar")
+        self.btn_crear = QtGui.QPushButton(Form)
+        self.btn_crear.setGeometry(QtCore.QRect(130, 20, 98, 27))
+        self.btn_crear.setObjectName("btn_crear")
+        self.btn_filtro = QtGui.QPushButton(Form)
+        self.btn_filtro.setGeometry(QtCore.QRect(10, 100, 98, 27))
+        self.btn_filtro.setObjectName("btn_filtro")
+        self.filtro_actor = QtGui.QLineEdit(Form)
+        self.filtro_actor.setGeometry(QtCore.QRect(130, 100, 201, 27))
+        self.filtro_actor.setObjectName("filtro_actor")
+        self.label = QtGui.QLabel(Form)
+        self.label.setGeometry(QtCore.QRect(380, 300, 141, 17))
+        self.label.setObjectName("label")
+        self.label_desc = QtGui.QLabel(Form)
+        self.label_desc.setMaximumSize(QtCore.QSize(200, 16777215))
+        self.label_desc.setGeometry(QtCore.QRect(380, 330, 141, 91))
+        self.label_desc.setObjectName("label_desc")
+        self.label_desc.setWordWrap(True)
+        self.label_reparto = QtGui.QLabel(Form)
+        self.label_reparto.setGeometry(QtCore.QRect(380, 206, 141, 81))
+        self.label_reparto.setObjectName("label_reparto")
+        self.label_4 = QtGui.QLabel(Form)
+        self.label_4.setGeometry(QtCore.QRect(380, 170, 131, 17))
+        self.label_4.setObjectName("label_4")
+        self.label_imagen = QtGui.QLabel(Form)
+        self.label_imagen.setGeometry(QtCore.QRect(380, 30, 141, 121))
+        self.label_imagen.setObjectName("label_imagen")
+        self.grilla = QtGui.QTableView(Form)
+        self.grilla.setGeometry(QtCore.QRect(10, 150, 341, 281))
+        self.grilla.setObjectName("grilla")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QtGui.QApplication.translate("Form", "Form", None, QtGui.QApplication.UnicodeUTF8))
+        self.btn_eliminar.setText(QtGui.QApplication.translate("Form", "Eliminar", None, QtGui.QApplication.UnicodeUTF8))
+        self.btn_editar.setText(QtGui.QApplication.translate("Form", "Editar", None, QtGui.QApplication.UnicodeUTF8))
+        self.btn_crear.setText(QtGui.QApplication.translate("Form", "Crear", None, QtGui.QApplication.UnicodeUTF8))
+        self.btn_filtro.setText(QtGui.QApplication.translate("Form", "Filtro", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("Form", "Descripcion", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_desc.setText(QtGui.QApplication.translate("Form", "TextLabel", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_reparto.setText(QtGui.QApplication.translate("Form", "TextLabel", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_4.setText(QtGui.QApplication.translate("Form", "Reparto", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_imagen.setText(QtGui.QApplication.translate("Form", "TextLabel", None, QtGui.QApplication.UnicodeUTF8))
+
---
+++ Peliculas/controladorPeliculas.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+from PySide import QtGui, QtCore
+from Peliculas import Ui_Form
+import model   #cambiar el nombre del model conforme a lo que se necesite
+
+
+class Movies(QtGui.QMainWindow):
+
+    table_columns = (
+        (u"Título", 200),
+        (u"Estreno", 75),
+        (u"Pais", 120),
+        (u"Descripcion", 75),
+        (u"Director_id", 60)) #por ahora, luego remplazar por solo director
+
+    def __init__(self, parent=None):
+        QtGui.QMainWindow.__init__(self, parent)
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+        self.load_movies()
+        self.show()
+        self.signals()
+
+
+    def signals(self):
+        self.ui.grilla.clicked.connect(self.show_poster) #table_movies es temporal
+        self.ui.btn_eliminar.clicked.connect(self.delete)            #btn_up es temporal
+    def delete(self):
+        index=self.ui.grilla.currentIndex()
+        data = self.ui.grilla.model()
+        mov = data.item(index.row(), 0).mov
+        model.delete_movie(mov["nombre"])
+        self.load_movies()
+        return
+    def show_poster(self, index):
+        index = index if index is not None\
+            else self.ui.grilla.currentIndex()
+        if index.row() == -1:
+            QtGui.QMessageBox.information(
+                None,
+                u"Información",
+                u"Por favor seleccione una orden de trabajo.")
+            return
+        data = self.ui.grilla.model()
+        mov = data.item(index.row(), 0).mov
+
+        # Colocamos la información en los labels
+  #      self.ui.lbl_stars.setText(mov["stars"])
+        self.ui.label_desc.setText(mov["descripcion"])  #recordar cambiar el label.setWordWrap True para reparto :D
+        # Ahora la imagen
+  #      img = QtGui.QPixmap(str(mov['poster']))  #str[mov[poster]] da el nombre del archivo para luego usarlo como una imagen
+  #      self.ui.lbl_image.setPixmap(img)
+
+    def load_movies(self):
+        movies = model.get_movies() #cambiar el nombre del model dependiendo de lo que se necesite
+        rows = len(movies)
+        data = QtGui.QStandardItemModel(
+            rows, len(self.table_columns))
+        self.ui.grilla.setModel(data)
+        self.ui.grilla.horizontalHeader().setResizeMode(
+            0, self.ui.grilla.horizontalHeader().Stretch)
+
+        for col, h in enumerate(self.table_columns):
+            data.setHeaderData(col, QtCore.Qt.Horizontal, h[0])
+            self.ui.grilla.setColumnWidth(col, h[1])
+
+        for i, mov in enumerate(movies):
+            row = [
+                mov["nombre"], mov["estreno"], mov["pais"],  # cambiar lo que esta dentro del parentesis dependiendo de la tabla
+                mov["descripcion"], mov["director_id"]]
+            for j, field in enumerate(row):
+                index = data.index(i, j, QtCore.QModelIndex())
+                data.setData(index, field)
+            # Parametros ocultos
+            data.item(i).mov = mov
+
+
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    main = Movies()
+    sys.exit(app.exec_())
