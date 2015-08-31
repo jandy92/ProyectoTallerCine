64484031ab4d3460c2bb23dcdee92cf469310d9c
Alejandra <ale_hg13@hotmail.com>
2015-08-31 09:41:46 -0300

    Implementado controlador para agregar nuevo director, solo falta lo de la imagen

--- Director/Controlador_form_crear_director.py
+++ Director/Controlador_form_crear_director.py
@@ -13,6 +13,7 @@ class Controlador_form_crear_director(QtGui.QMainWindow):
 		self.ui.setupUi(self)
 		self.show()
 		self.signals()
+		self.listo=False;#True: los datos obligatorios existen, False:si no
 		self.nombre=""
 		self.imagen=""
 		self.nacimiento=""
@@ -21,20 +22,37 @@ class Controlador_form_crear_director(QtGui.QMainWindow):
 
     def signals(self):
     	self.ui.difunto_check.clicked.connect(self.difunto_check_clicked)
-
+    	self.ui.crear_boton.clicked.connect(self.crear_director)
 
     def difunto_check_clicked(self):#si "difunto" esta chequeado activa el ingreso de fecha de defuncion, de lo contrario, la desactiva
     	#print(self.ui.difunto_check.isChecked())
     	self.ui.defuncion_in.setEnabled(self.ui.difunto_check.isChecked())
-    	self.obtener_datos()
 
     def obtener_datos(self):
+    	self.nombre=""
+    	self.imagen=""
+    	self.nacimiento=""
+    	self.defuncion=""
+    	self.pais=""
     	self.nombre=self.ui.nombre_in.text()#obligatorio
     	self.pais=self.ui.pais_in.text()#obligatorio
     	self.nacimiento=self.ui.nacimiento_in.date().toPython().strftime("%Y-%m-%d")#transformar de fecha en QT a fecha en python a string
-    	self.defuncion=self.ui.defuncion_in.date().toPython().strftime("%Y-%m-%d")#transformar de fecha en QT a fecha en python a string
-    	#print(self.nacimiento)
+    	if(self.ui.difunto_check.isChecked()):
+    		self.defuncion=self.ui.defuncion_in.date().toPython().strftime("%Y-%m-%d")#transformar de fecha en QT a fecha en python a string
+    	self.listo=False
+    	print(len(self.nacimiento))
+    	if(len(self.nombre)!=0 and len(self.nacimiento)!=0):#True: si los campos obligatorios estan definidos, False: si no
+    		self.listo=True
 
+    def crear_director(self):
+    	self.obtener_datos()
+    	if(self.listo==True):#si los campos obligatorios tienen datos, se crea el director
+    		print("Creando nuevo director ...")
+    		               #crear_director(nombre, pais, fecha_nacimiento,fecha_defuncion):
+    		Modelo_director.crear_director(self.nombre,self.pais,self.nacimiento,self.defuncion)
+    	else:#si falta algun campo obligatorio, no se creara el nuevo director
+    		print("Faltan datos obligatorios, no se ha creado un nuevo director.")
+    	
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
