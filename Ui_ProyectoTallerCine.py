ff2c6f78ada7cdebd25b66593e10b3b79950c897
Alejandra <ale_hg13@hotmail.com>
2015-09-01 11:22:31 -0300

    elementos graficos re-editados#2

--- Ui_ProyectoTallerCine.py
+++ Ui_ProyectoTallerCine.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ProyectoTallerCine2.ui'
 #
-# Created: Tue Sep  1 09:25:55 2015
+# Created: Tue Sep  1 11:20:09 2015
 #      by: pyside-uic 0.2.15 running on PySide 1.2.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -14,29 +14,34 @@ class Ui_Form(object):
         Form.setObjectName("Form")
         Form.resize(400, 300)
         self.Director = QtGui.QPushButton(Form)
-        self.Director.setGeometry(QtCore.QRect(150, 210, 101, 29))
+        self.Director.setGeometry(QtCore.QRect(150, 230, 101, 29))
         self.Director.setObjectName("Director")
         self.Actores = QtGui.QPushButton(Form)
-        self.Actores.setGeometry(QtCore.QRect(20, 210, 106, 29))
+        self.Actores.setGeometry(QtCore.QRect(20, 230, 106, 29))
         self.Actores.setObjectName("Actores")
         self.Peliculas = QtGui.QPushButton(Form)
-        self.Peliculas.setGeometry(QtCore.QRect(280, 210, 106, 29))
+        self.Peliculas.setGeometry(QtCore.QRect(280, 230, 106, 29))
         self.Peliculas.setObjectName("Peliculas")
         self.label = QtGui.QLabel(Form)
-        self.label.setGeometry(QtCore.QRect(-10, 70, 411, 121))
+        self.label.setGeometry(QtCore.QRect(-10, 80, 411, 121))
         self.label.setText("")
         self.label.setPixmap(QtGui.QPixmap("fondo.jpg"))
         self.label.setScaledContents(True)
         self.label.setObjectName("label")
         self.label_2 = QtGui.QLabel(Form)
-        self.label_2.setGeometry(QtCore.QRect(20, 10, 381, 51))
+        self.label_2.setGeometry(QtCore.QRect(30, 10, 441, 51))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setWeight(75)
+        font.setBold(True)
+        self.label_2.setFont(font)
         self.label_2.setObjectName("label_2")
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
 
     def retranslateUi(self, Form):
-        Form.setWindowTitle(QtGui.QApplication.translate("Form", "Form", None, QtGui.QApplication.UnicodeUTF8))
+        Form.setWindowTitle(QtGui.QApplication.translate("Form", "Menú Principal", None, QtGui.QApplication.UnicodeUTF8))
         self.Director.setText(QtGui.QApplication.translate("Form", "Director", None, QtGui.QApplication.UnicodeUTF8))
         self.Actores.setText(QtGui.QApplication.translate("Form", "Actores", None, QtGui.QApplication.UnicodeUTF8))
         self.Peliculas.setText(QtGui.QApplication.translate("Form", "Peliculas", None, QtGui.QApplication.UnicodeUTF8))
